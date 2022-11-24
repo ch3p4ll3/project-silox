b38ecb44b763097fa5a11b7dc5343472cbbfd1ce
Mattia <vidonimattia@gmail.com>
2022-11-24 17:07:30 +0100

    added worker_status to silos model

--- backend/api/apps/api/models/silos.py
+++ backend/api/apps/api/models/silos.py
@@ -3,6 +3,7 @@ from .zones import Zones
 from .liquids import Liquids
 from .actions import Actions
 from ...utils.influx_management import InfluxDb
+from django.conf import settings
 
 
 class Silos(models.Model):
@@ -13,9 +14,13 @@ class Silos(models.Model):
     actions = models.ManyToManyField(Actions, blank=True)
 
     def lastmeasurement(self) -> dict:
-        # prendere ultima misurazione da influx
         data = InfluxDb().read(self, last=True)
         return data
+
+    def is_worker_running(self):
+        worker = next((i for i in settings.SIMS if i.silos.id == self.id), None)
+
+        return worker is not None
 
     def __str__(self):
         return f"silos#{self.id}"
