3f8e064e89114478f5eb2992febcc58555429080
Mattia <vidonimattia@gmail.com>
2022-12-09 12:28:59 +0100

    https, nginx reverse proxy

--- backend/api/project_silos/settings.py
+++ backend/api/project_silos/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['api.projectsilox.ml']
+CSRF_TRUSTED_ORIGINS = ['https://api.projectsilox.ml']
 
 
 # Application definition
