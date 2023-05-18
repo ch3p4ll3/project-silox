b1a6480abdc3e6690768de25c3225862db83b34e
Mattia <vidonimattia@gmail.com>
2023-05-18 16:13:39 +0200

    fix liquid property

--- backend/api/apps/api/serializers/liquids_properties_serializer.py
+++ backend/api/apps/api/serializers/liquids_properties_serializer.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from ..models import LiquidProperties
-from .properties_serializer import PropertiesSerializer
 
 
 class LiquidsPropertiesSerializer(serializers.ModelSerializer):
-    #property = PropertiesSerializer(read_only=True)
-
     class Meta:
         model = LiquidProperties
         fields = '__all__'
--- backend/api/apps/api/serializers/liquids_serializer_get.py
+++ backend/api/apps/api/serializers/liquids_serializer_get.py
@@ -1,27 +1,10 @@
 from rest_framework import serializers
 from ..models import Liquids
-from ..models import LiquidProperties
 from .liquids_properties_serializer_get import LiquidsPropertiesSerializerGet
 
 
 class LiquidsSerializerGet(serializers.ModelSerializer):
     properties = LiquidsPropertiesSerializerGet(many=True, required=False)
-
-    def create(self, validated_data: dict):
-        liquid_properties = validated_data.pop("properties", [])
-
-        liquid_model = Liquids(**validated_data)
-        liquid_model.save()
-
-        for liquid_property in liquid_properties:
-            prop, _ = LiquidProperties.objects.get_or_create(**liquid_property)
-            prop.save()
-
-            liquid_model.properties.add(prop)
-
-        liquid_model.save()
-
-        return liquid_model
 
     class Meta:
         model = Liquids
