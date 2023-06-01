a4f0b2286c3334ee6a682529bf46e44781b7fa88
Mattia <vidonimattia@gmail.com>
2023-06-01 09:17:38 +0200

    sentry sdk, github CI/CD

--- backend/api/project_silos/settings/common.py
+++ backend/api/project_silos/settings/common.py
@@ -1,4 +1,22 @@
 from os import getenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=getenv("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 DEBUG = getenv("DEBUG", 'true').lower() == 'true'  # If DEBUG is not set, default to True (development mode)
 
